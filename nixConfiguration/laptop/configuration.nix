017c25590f4927e8fad97fbb291c6e79c85a86e1
Matthew Stewart <m.scott.stewart@gmail.com>
2020-05-02 21:24:18 -0400

    Switched to nVidia optimus prime sync mode.  Offloat doesnt work well.

--- nixConfiguration/laptop/configuration.nix
+++ nixConfiguration/laptop/configuration.nix
@@ -1,14 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
-in
 {
   imports =
     [
@@ -37,12 +28,14 @@ in
     wireless.enable = true; # Enables wireless support via wpa_supplicant.
   };
 
-  environment.systemPackages = [ nvidia-offload ];
+  hardware.nvidia = {
+    modesetting.enable = true;
 
-  hardware.nvidia.prime = {
-    offload.enable = true;
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   services = {
